2fa9f7c072554fce3760f0d636028d6f4d24facd
yuichan2122 <33638496+yuichan2122@users.noreply.github.com>
2020-02-28 18:08:46 -0800

    90 days without internet connection

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -24,8 +24,8 @@ FW_SIGNATURE = get_expected_signature()
 ThermalStatus = log.ThermalData.ThermalStatus
 NetworkType = log.ThermalData.NetworkType
 CURRENT_TAU = 15.   # 15s time constant
-DAYS_NO_CONNECTIVITY_MAX = 7  # do not allow to engage after a week without internet
-DAYS_NO_CONNECTIVITY_PROMPT = 4  # send an offroad prompt after 4 days with no internet
+DAYS_NO_CONNECTIVITY_MAX = 90  # do not allow to engage after a week without internet
+DAYS_NO_CONNECTIVITY_PROMPT = 80  # send an offroad prompt after 4 days with no internet
 
 
 with open(BASEDIR + "/selfdrive/controls/lib/alerts_offroad.json") as json_file:
